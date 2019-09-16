f0342427921f3e1f23e4f6ca510a7f162446c979
Recito Gigih P <recitogigih9@gmail.com>
2019-09-16 23:56:20 +0700

    init

---
+++ components/Navigation/TheHeader.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="header-container">
+    <header class="the-header" :class="{ 'the-header--hidden': !showNavbar }" id="nav">
+      <TheSideNavToggle @toggle="$emit('sidenavToggle')" />
+      <div class="logo ">
+        <nuxt-link to="/"><img class="is-vertical-center" src="alexatech.png" alt="Xurya Logo">
+        </nuxt-link>
+      </div>
+      <div class="spacer"></div>
+      <div class="navigation-items">
+        <ul class="nav-list">
+          <li class="nav-item">
+            <nuxt-link to="/" exact>Home</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/rooftop-solar">
+              Rooftop Solar
+            </nuxt-link>
+          </li>
+           <li class="nav-item">
+            <nuxt-link to="/about-us" exact>About Us</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/xurya-lease">
+              Xurya Lease
+            </nuxt-link>
+             <!-- <a target="blank" href="https://getsolar.xurya.com/">
+              Xurya Lease
+            </a> -->
+          </li>
+          <li class="nav-item">
+            <!-- <nuxt-link to="/calculator">
+              Xurya Calculator
+            </nuxt-link > -->
+            <a target="blank" href="https://calculator.xurya.com/">
+              Xurya Calculator
+            </a>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/contact">
+              Contact
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+  import TheSideNavToggle from "@/components/Navigation/TheSideNavToggle";
+
+  export default {
+    name: "TheHeader",
+    components: {
+      TheSideNavToggle
+    },
+    data() {
+      return {
+        showNav: false,
+        showNavbar: true,
+        lastScrollPosition: 0
+      };
+    },
+    mounted() {
+      window.addEventListener("scroll", this.onScroll);
+      this.$nextTick(function () {
+        window.addEventListener("scroll", function () {
+          var navbar = document.getElementById("nav")
+          var nav_classes = navbar.classList
+          if (document.documentElement.scrollTop >= 150 || document.body.scrollTop >= 150) {
+            if (nav_classes.contains("shrink") === false) {
+              nav_classes.toggle("shrink");
+            }
+          } else {
+            if (nav_classes.contains("shrink") === true) {
+              nav_classes.toggle("shrink");
+            }
+          }
+        })
+      })
+    },
+    beforeDestroy() {
+      window.removeEventListener("scroll", this.onScroll);
+    },
+
+    methods: {
+      onScroll() {
+        // Get the current scroll position
+        const currentScrollPosition =
+          window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+        // Because of momentum scrolling on mobiles, we shouldn't continue if it is less than zero
+        if (currentScrollPosition < 0) {
+          return;
+        }
+        // Here we determine whether we need to show or hide the navbar
+        this.showNavbar = currentScrollPosition < this.lastScrollPosition;
+        // Set the current scroll position as the last scroll position
+        this.lastScrollPosition = currentScrollPosition;
+      }
+    }
+  };
+
+</script>
+
+
+<style scoped>
+  .the-header.shrink {
+    transition: all 0.5s;
+    background: white;
+    -webkit-box-shadow: 0px 18px 37px -13px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 0px 18px 37px -13px rgba(0, 0, 0, 0.75);
+    box-shadow: 0px 18px 37px -13px rgba(0, 0, 0, 0.75);
+    /* color: black !important; */
+  }
+
+  .the-header.the-header--hidden {
+    box-shadow: none;
+    transform: translate3d(0, -100%, 0);
+
+  }
+
+  /* .header-container {
+    height: 60px;
+  } */
+
+  .the-header {
+    width: 100%;
+    position: fixed;
+    height: 60px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    /* background-color: rgb(255, 255, 255); */
+    /* background: -webkit-linear-gradient(top, rgb(255, 255, 255) 0%, rgba(255, 255, 255, 0.8) 65%, rgba(255, 255, 255, 0.5) 88%, rgba(255, 255, 255, 0.3) 100%); */
+    z-index: 100;
+    box-sizing: border-box;
+    padding: 0 20px;
+    transform: translate3d(0, 0, 0);
+    transition: 0.1s all ease-out;
+    transition: all 0.5s;
+
+  }
+
+  .logo {
+    width: 130px;
+  }
+
+  .logo a {
+    text-decoration: none;
+  }
+
+  .spacer {
+    flex: 1;
+  }
+
+  .navigation-items {
+    display: none;
+  }
+
+  @media (min-width: 768px) {
+    .navigation-items {
+      display: block;
+    }
+  }
+
+  .nav-list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+  }
+
+  .nav-item {
+    margin: 0 10px;
+  }
+
+  .nav-item a {
+    text-decoration: none;
+    color: black;
+  }
+
+  .nav-item a:hover,
+  .nav-item a:active,
+  .nav-item a.nuxt-link-active {
+    color: #FFAB06;
+  }
+
+  .is-vertical-center {
+    display: flex;
+    align-items: center;
+  }
+
+</style>
---
+++ components/Navigation/TheSidenav.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="sidenav-container">
+    <div v-if="show" class="sidenav-backdrop" @click="$emit('close')"></div>
+    <transition name="slide-side">
+      <div v-if="show" class="sidenav">
+
+        <div class="close" @click="$emit('close')" style="position:absolute;right:8%;top:2%;cursor:pointer;"> <i class="fa fa-2x fa-times" aria-hidden="true"></i>
+        </div>
+        <ul class="nav-list" @click="$emit('close')">
+          <nuxt-link to="/" class="has-text-dark">
+            <li class="nav-item">
+              Home
+            </li>
+          </nuxt-link>
+
+          <li class="nav-item">
+            <nuxt-link to="/rooftop-solar">
+              Rooftop Solar
+            </nuxt-link>
+          </li>
+          <nuxt-link to="/About-us" class="has-text-dark">
+            <li class="nav-item">
+              About Us
+            </li>
+          </nuxt-link>
+          <nuxt-link to="/xurya-lease" class="has-text-dark">
+            <li class="nav-item">
+              <!-- <a target="blank" href="https://getsolar.xurya.com/">
+              Xurya Lease
+            </a> -->
+              Xurya Lease
+            </li>
+          </nuxt-link>
+        <a class="has-text-dark" target="blank" href="https://calculator.xurya.com/">
+            <li class="nav-item">
+              <!-- <a target="blank" href="https://calculator.xurya.com/">
+              Xurya Calculator
+            </a> -->
+              Xurya Calculator
+            </li>
+         </a>
+          <nuxt-link to="/contact" class="has-text-dark">
+            <li class="nav-item">
+              Contact
+            </li>
+          </nuxt-link>
+        </ul>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TheSidenav",
+    props: {
+      show: {
+        type: Boolean,
+        default: false
+      }
+    }
+  };
+
+</script>
+
+
+<style scoped>
+  .sidenav-container {
+    height: 100%;
+    width: 100%;
+  }
+
+  .sidenav-backdrop {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    z-index: 1000;
+    position: fixed;
+    top: 0;
+    right: 0;
+  }
+
+  .sidenav {
+    height: 100%;
+    width: 50%;
+    background-color: white;
+    z-index: 10000;
+    position: fixed;
+    top: 0;
+    right: 0;
+    box-sizing: border-box;
+    padding: 30px;
+  }
+
+  .slide-side-enter-active,
+  .slide-side-leave-active {
+    transition: all 0.3s ease-out;
+  }
+
+  .slide-side-enter,
+  .slide-side-leave-to {
+    transform: translateX(100%);
+  }
+
+  .nav-list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .nav-item {
+    margin: 20px 0;
+  }
+
+  .nav-item a {
+    text-decoration: none;
+    color: black;
+    /* font-size: 1.5rem; */
+  }
+
+  .nav-item a:hover,
+  .nav-item a:active {
+    color: #FFAB06;
+  }
+
+</style>
