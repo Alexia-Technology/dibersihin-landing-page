cef7a2efd61f590d12d658c13aee015d4e31eb58
recitogigih <recitogigih9@gmail.com>
2020-08-06 12:59:40 +0700

    lnjutan pp

--- components/privacy_policy.vue
+++ components/privacy_policy.vue
@@ -34,6 +34,83 @@
               <li> How to contact us</li>
             </ul>
           </div>
+
+          <div class="content">
+            <ol type="1">
+              <li>
+                <h1
+                  class=" m-b-40 title has-text-black is-size-4-desktop is-size-4-tablet is-size-4-mobile has-text-center">
+                  <b>Personal Information which we collect</b></h1>
+                <p>We collect information which identifies or can be used to identify, contact, or locate the person or
+                  device to whom that information pertains (“Personal Information”). Personal Information includes, but
+                  not limited to, name, address, date of birth, occupation, phone number, e-mail address, bank account
+                  and credit-card details, gender, identification (including KTP, SIM, or Passport) or other government
+                  issued identifier, vehicle information, photo, nationality, phone number of our users and non-users in
+                  your mobile phonebook, health data, financial related information, and biometric information
+                  (including but not limited to facial recognition). Additionally, to the extent other information,
+                  including a personal profile, and/or unique identifier, is associated or combined with Personal
+                  Information, then that information is also Personal Information. The Personal Information which we
+                  collect may be provided by you directly or by third parties (for example: when you register or use the
+                  Application, when you contact our customer services, or you otherwise provide Personal Information to
+                  us). We may collect information in various forms and purposes (including purposes permitted under
+                  applicable law).</p>
+                <h1
+                  class=" m-b-40 title has-text-black is-size-4-desktop is-size-4-tablet is-size-4-mobile has-text-center">
+                  <b>Information obtained from you or from your mobile device directly</b></h1>
+                <div class="content">
+                  <ul>
+                    <li>When you register and create an account with us using the Application you have to provide to us
+                      certain Personal Information, including your name, physical address, e-mail address and your phone
+                      number. If you are using the Application as a service provider, you have to provide us with
+                      additional Personal Information as part of the service provider onboarding process. This would
+                      include details of your vehicle (licenses, approvals, and other authorizations for you to operate
+                      the vehicle and to provide the transportation services), your insurance policy, and your bank
+                      account.</li>
+                  </ul>
+                </div>
+              </li>
+              <br>
+              <li>
+                <h1
+                  class=" m-b-40 title has-text-black is-size-4-desktop is-size-4-tablet is-size-4-mobile has-text-center">
+                  <b>THE USE OF PERSONAL INFORMATION WHICH WE COLLECT</b></h1>
+                <p>We may use Personal Information collected for any of the following purposes as well as for such other
+                  purposes as are permitted by applicable law (“Purposes”):</p>
+                <div class="content">
+                  <ul>
+                    <li>to identify you and to register you as a user and to administer, verify, deactivate, or manage
+                      your account as such;</li>
+                    <li>to facilitate or enable any verification as we may in our discretion consider necessary before
+                      the service provider provide you with the services or before we register you as a user, including
+                      performing Know Your Customer (KYC) and credit scoring;</li>
+                    <li>to enable service providers to provide you with such of the services as you have requested;
+                    </li>
+                    <li>to process and facilitate orders and payment transactions made by you, including where
+                      applicable, transactions made through any payment card or account available over the Application;
+                    </li>
+                    <li>to notify you of any transaction or activities occurred within the Application or other system
+                      linked to our Application;</li>
+                    <li>to communicate with you and to send you information in connection with the use of the
+                      Application; </li>
+                    <li>to notify you of any updates to the Application or changes to the services available; </li>
+                    <li>to process and respond to enquiries and feedback received from you; </li>
+                    <li>to maintain, develop, test, enhance and personalize the Application to meet your needs and
+                      preferences as a user;</li>
+                    <li>to monitor and analyse user activities, behaviour, and demographic data including trends and
+                      usage of the various services available on the Application; </li>
+                    <li>to process and manage your reward points;</li>
+                    <li>to offer or provide services from our affiliates or partners; and </li>
+                    <li>to send you direct or targeted marketing communications, advertisement, vouchers, surveys, and
+                      information on special offers or promotions.</li>
+                  </ul>
+                </div>
+              </li>
+              <br>
+              <li> <h1
+                  class=" m-b-40 title has-text-black is-size-4-desktop is-size-4-tablet is-size-4-mobile has-text-center">
+                  <b>Information obtained from you or from your mobile device directly</b></h1></li>
+            </ol>
+          </div>
         </div>
       </div>
     </section>
