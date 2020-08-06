c135380808b04172159220747995b5e34acd96d4
recitogigih <recitogigih9@gmail.com>
2020-08-06 12:42:05 +0700

    pp

--- components/privacy_policy.vue
+++ components/privacy_policy.vue
@@ -5,6 +5,14 @@
         <div class="hero-body">
           <h1 class=" m-b-40 title has-text-black is-size-3-desktop is-size-3-tablet is-size-4-mobile has-text-center">
             <b>PRIVACY POLICY</b></h1>
+  <p>
+PRIVACY POLICY
+as of 30 July 2020
+The following Privacy Policy describes how we, dibersihin (PT Digawean Integrasi Teknologi and affiliates, “we, “us” or “our”) collect, store, use, process, retain, transfer, disclose and protect your Personal Information. This Privacy Policy applies to all users of our applications, websites (www.dibersihin.com), services, or products (“Applications”), unless covered by a separate privacy policy.
+
+Please read this Privacy Policy thoroughly to ensure that you understand our data processing practices. Unless otherwise defined, all capitalized terms used in this Privacy Policy shall have the same meaning ascribed to them in the Terms of Use.
+
+This Privacy Policy includes the following matters:</p>
         </div>
       </div>
     </section>
